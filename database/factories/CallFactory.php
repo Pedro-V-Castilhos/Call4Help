95271d1264001694552254abe8e394e77878ed2e
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-04-30 17:40:14 -0300

    :heavy_plus_sign: build: creating the model factories

---
+++ database/factories/CallFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Call;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Call>
+ */
+class CallFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
